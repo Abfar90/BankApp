6c611efb46f1e1775274024c2c4f9a5e5813985e
Abdi Farah <abdirahman.farah@live.se>
2023-01-04 19:21:56 +0100

    Add project files.

---
+++ Classes/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken2023.Classes
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public int Balance { get; set; }
+        public string AccountType { get; set; }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+        public void MakeDeposit()
+        {
+
+        }
+
+        public void MakeTransaction()
+        {
+
+        }
+
+        public Account(int accountNumber, int balance, string accountType)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            AccountType = accountType;
+        }
+    }
+}
